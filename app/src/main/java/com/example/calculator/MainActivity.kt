c3d4f16afb8d52de94da5bc08e5672298f7c27b3
ivjdragon <meliodasdragonsin008@gmail.com>
2022-10-23 17:52:45 +0300

    added a comment of the icon

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -14,3 +14,9 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+                //CHANGING THE ICON
+/*
+To change the icon, i right clicked on the app and selected image vector and downloaded a png photo.
+NB/ DON'T NAME THE PNG WITH SPACE BETWEEN THE WORDS AND DELETE THE ORIGINAL ICON IN MIPMAP LEAVING
+YOUR ICON AS THE ONLY FOLDER IN MIPMAP
+*/
